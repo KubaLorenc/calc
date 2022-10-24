90fddb316aa4c9da726aa1ab35a26fb7b38e52b6
Jakub Lorenc <jakub.lorenc@volue.com>
2022-10-24 23:55:14 +0200

    init

---
+++ ConsoleApp5/CalculatorService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class CalculatorCommand
+    {
+        private readonly ICalculator _calculator;
+        public CalculatorCommand(ICalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public int Call(string[] args)
+        {
+            var operationType = DetermineOperationType(args[0]);
+            return _calculator.Calculate(operationType, 1, 2);
+        }
+
+        public IOperation DetermineOperationType(string input)
+        {
+            if (!Enum.TryParse(typeof(Operations), input.ToString().ToUpper(), out object operationType))
+            {
+                throw new Exception();
+            }
+            switch ((Operations)operationType)
+            {
+                case Operations.Add:
+                    return new Adding();
+                default:
+                    return new Adding();
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new CalculatorCommand(new Calculator(), new EnumHelper());
+            Console.WriteLine(service.Call(args));
+        } 
+
+
+    }
+    public enum Operations
+    {
+        Undefined,
+        Add,
+        Subtract,
+        Multiply
+    }
+
+    public interface IOperation
+    {
+        public int Result(int input1, int input2);
+    }
+
+    public interface ICalculator
+    {
+        public int Calculate(IOperation operation, int input1, int input2);
+    }
+    internal class Adding : IOperation
+    {
+        public int Result(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+    }
+    internal class Calculator : ICalculator
+    {
+        public int Calculate(IOperation operation, int input1, int input2)
+        {
+            return operation.Result(input1, input2);
+        }
+    }
+}
